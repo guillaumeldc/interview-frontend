3f7bb89277134948e48c06deb253fdae389b5f1f
dd <4daveid@gmail.com>
2023-01-30 16:30:37 -0500

    added countries selector, display companies by country, slightly design changes, added scss style support, proxy middlware,

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as mainService from './services/main.service';
+import logo from './logo-main.svg';
+import './App.scss';
 
 function App() {
+  const [countriesList, setCountriesList] = useState<any>([]);
+  const [companiesList, setCompaniesList] = useState<any>([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedOffice, setSelectedOffice] = useState('');
+
+  useEffect(() => {
+    mainService.getAllCountries()
+      .then(resp => setCountriesList(resp));
+
+    // mainService.getAllCities()
+    //   .then(resp => setCitiesList(resp));
+  }, []);
+
+  const handleCountrySelected = (e: any) => {
+    const selectedCountryName = e.target.value;
+    setSelectedCountry(selectedCountryName);
+  }
+
+  const handleOfficeSelected = (officeName: string) => {
+    setSelectedOffice(officeName);
+  }
+
+  useEffect(() => {
+    if (selectedCountry) {
+      mainService.getAllCompanies({ city__country__name: selectedCountry })
+        .then(resp => setCompaniesList(resp))
+    }
+  }, [selectedCountry])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          testing react with eDigital
-        </a>
-      </header>
+    <div className="app">
+      <div className="page-holder">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+
+        <section>
+          <div className='form-holder'>
+            {selectedOffice && <fieldset>
+              <h4>Welcome to {selectedOffice}!</h4>
+            </fieldset>}
+
+            <fieldset>
+              <h4>Please select country</h4>
+            </fieldset>
+
+            <fieldset>
+              <select onChange={handleCountrySelected}>
+                {
+                  countriesList.map(({ id, name }: any) =>
+                    <option key={id}>{name}</option>
+                  )
+                }
+              </select>
+            </fieldset>
+
+            {
+              selectedCountry && <>
+                <fieldset>
+                  <h4>Now please select the company!</h4>
+                </fieldset>
+                <fieldset>
+                  <ul>
+                    {
+                      companiesList.map(({ id, name }: any, i: number) =>
+                        <li key={id}
+                          onClick={() => handleOfficeSelected(name)}>
+                          {name}
+                        </li>
+                      )
+                    }
+                  </ul>
+                </fieldset>
+              </>
+            }
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
